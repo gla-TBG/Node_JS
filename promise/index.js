c7cf622c65af808566b57c0bc9c640cdecfa19df
zhongjunqi <zhongjunqi@iplas.com.cn>
2020-06-12 09:51:55 +0800

    promise

---
+++ promise/index.js
@@ -0,0 +1,19 @@
+
+function interview(count){
+    return new Promise((reslove, reject) => {
+        if (Math.random() > 0.2) {
+            reslove('success')
+        } else {
+            reject('fail at'+ count)
+        }
+    })
+}
+
+interview(1).then(()=> {
+    return interview(2)
+}).then((res)=>{
+    console.log(res)
+    return interview(3)
+}).catch(err=>{
+    console.log(err)
+})
